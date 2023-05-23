25c50206124311a222ce34bdf8af9f1ea8a23bb2
Timothy Cronin <tcroniniv@gmail.com>
2023-05-23 14:22:00 -0400

    add new spacers

--- src/formats.rs
+++ src/formats.rs
@@ -4,5 +4,7 @@ pub static RED_FG: &str = "\x1b[31m";
 pub static BLUE_FG: &str = "\x1b[34m";
 pub static BOLD: &str = "\x1b[1m";
 pub static RESET: &str = "\x1b[0m";
-pub static BRANCH_CHAR: &str = "└──";
-// ├
+pub static BRANCH_HAS_NEXT: &str = "├──";
+pub static BRANCH_END: &str = "└──";
+pub static VER_LINE_SPACER: &str = "│  ";
+pub static SPACER: &str = "   ";
