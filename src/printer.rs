56d1eb0f2d620314909d26ffa7f749f63c9ca378
Timothy Cronin <tcroniniv@gmail.com>
2023-05-23 11:18:23 -0400

    better formatting

--- src/printer.rs
+++ src/printer.rs
@@ -6,29 +6,29 @@ pub fn print_directory(dir_ptr: DirPointer, depth: usize) {
 
     // let new_d_ref = d_ref.borrow().parent.clone().unwrap();
     // d_ref = new_d_ref
-    for _ in 0..depth * 2 {
-        print!(" ");
+    for _ in 0..depth {
+        print!("  ");
     }
     println!("Dir: {}", dir.name);
 
-    for file in &dir.found_files {
-        print_file(file, depth + 1);
-    }
     for child in dir.children.clone() {
         print_directory(child, depth + 1);
+    }
+    for file in &dir.found_files {
+        print_file(file, depth + 1);
     }
 }
 
 pub fn print_file(file: &File, depth: usize) {
-    for _ in 0..depth * 2 {
-        print!(" ");
+    for _ in 0..depth {
+        print!("  ");
     }
     println!("File: {}", file.name);
     for line in &file.lines {
-        for _ in 0..depth * 2 {
-            print!(" ");
+        for _ in 0..depth {
+            print!("  ");
         }
-        print!(" ");
+        print!("  ");
         println!("line: {}", line);
     }
 }
