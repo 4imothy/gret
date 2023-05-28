ab2e6387609c9af08b9840791aa89a6e821b0022
Timothy Cronin <tcroniniv@gmail.com>
2023-05-28 13:06:52 -0400

    clean up

--- src/errors.rs
+++ src/errors.rs
@@ -1,57 +1,55 @@
 // SPDX-License-Identifier: GPL-3.0
 
 use crate::formats;
+use lazy_static::lazy_static;
 use std::fmt;
 use std::path::PathBuf;
 
-pub enum Errors {
-    PathNotFound { cause: PathBuf },
-    CantCanonicalize { cause: PathBuf },
-    CantWrite,
-    CantCollect { cause: String },
-    UnknownArgument { cause: String },
-    UnableToReadDir { cause: PathBuf },
-}
-
-fn error_prefix() -> String {
-    format!(
+lazy_static! {
+    static ref ERROR_PREFIX: String = format!(
         "{}{}Error: {}",
         formats::RED_FG,
         formats::BOLD,
         formats::RESET
-    )
+    );
+}
+
+pub enum Errors {
+    PathNotFound { cause: PathBuf },
+    CantWrite,
+    UnknownArgument { cause: String },
+    CantGetName { cause: PathBuf },
 }
 
 impl fmt::Display for Errors {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let error_prefix = error_prefix();
         match &self {
             Errors::PathNotFound { cause } => {
                 write!(
                     f,
-                    "{}Path, {:?}, not found.",
-                    error_prefix,
-                    cause.as_os_str()
+                    "{}Path: `{}` was not found.",
+                    ERROR_PREFIX.to_string(),
+                    cause.to_string_lossy()
                 )
             }
             Errors::UnknownArgument { cause } => {
-                write!(f, "{}Unknown Argument, {}", error_prefix, cause)
-            }
-            Errors::CantWrite => {
-                write!(f, "{}Can't print to Stdout", error_prefix)
-            }
-            Errors::CantCanonicalize { cause } => {
                 write!(
                     f,
-                    "{}Cannot get the absolute path of: {:?}",
-                    error_prefix, cause
+                    "{}Unknown Argument, `{}`",
+                    ERROR_PREFIX.to_string(),
+                    cause
                 )
             }
-            Errors::CantCollect { cause } => {
-                write!(f, "{}Can't collect items of type: {}", error_prefix, cause)
+            Errors::CantWrite => {
+                write!(f, "{}Can't print to Stdout", ERROR_PREFIX.to_string())
             }
-            Errors::UnableToReadDir { cause } => {
-                write!(f, "{}Unable to read directory: {:?}", error_prefix, cause)
+            Errors::CantGetName { cause } => {
+                write!(
+                    f,
+                    "{}Can't get the name of entry: `{}`",
+                    ERROR_PREFIX.to_string(),
+                    cause.to_string_lossy()
+                )
             }
         }
     }
