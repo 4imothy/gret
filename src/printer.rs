0419fc8637865a7e3e1f51df1615ee80fbdda6c8
Timothy Cronin <tcroniniv@gmail.com>
2023-05-28 13:12:13 -0400

    add styling

--- src/printer.rs
+++ src/printer.rs
@@ -1,3 +1,4 @@
+use crate::formats::{BLUE_FG, BOLD, CYAN_FG, RESET as STYLE_RESET};
 use crate::formats::{BRANCH_END, BRANCH_HAS_NEXT, SPACER, VER_LINE_SPACER};
 use crate::searcher::DirPointer;
 use crate::searcher::Directory;
@@ -5,10 +6,13 @@ use crate::searcher::File;
 use crate::Errors;
 use std::io::{self, Write};
 
+static DIR_COLOR: &str = CYAN_FG;
+static FILE_COLOR: &str = BLUE_FG;
+
 pub fn start_print_directory(out: &mut io::StdoutLock, dir_ptr: DirPointer) -> Result<(), Errors> {
     let prefix = "".to_string();
     let dir = dir_ptr.borrow();
-    writeln!(out, "{}", dir.name).map_err(|_| Errors::CantWrite)?;
+    write_dir_name(out, &dir.name)?;
 
     handle_descendants(out, dir, prefix)?;
 
@@ -56,11 +60,12 @@ fn print_directory(
     let dir = dir_ptr.borrow();
 
     if parent_has_next {
-        writeln!(out, "{}{}{}", prefix, BRANCH_HAS_NEXT, dir.name)
-            .map_err(|_| Errors::CantWrite)?;
+        write!(out, "{}{}", prefix, BRANCH_HAS_NEXT).map_err(|_| Errors::CantWrite)?;
+        write_dir_name(out, &dir.name)?;
         prefix += VER_LINE_SPACER;
     } else {
-        writeln!(out, "{}{}{}", prefix, BRANCH_END, dir.name).map_err(|_| Errors::CantWrite)?;
+        write!(out, "{}{}", prefix, BRANCH_END).map_err(|_| Errors::CantWrite)?;
+        write_dir_name(out, &dir.name)?;
         prefix += SPACER;
     }
 
@@ -76,11 +81,12 @@ fn print_file(
     parent_has_next: bool,
 ) -> Result<(), Errors> {
     if parent_has_next {
-        writeln!(out, "{}{}{}", prefix, BRANCH_HAS_NEXT, file.name)
-            .map_err(|_| Errors::CantWrite)?;
+        write!(out, "{}{}", prefix, BRANCH_HAS_NEXT).map_err(|_| Errors::CantWrite)?;
+        write_file_name(out, &file)?;
         prefix += VER_LINE_SPACER;
     } else {
-        writeln!(out, "{}{}{}", prefix, BRANCH_END, file.name).map_err(|_| Errors::CantWrite)?;
+        write!(out, "{}{}", prefix, BRANCH_END).map_err(|_| Errors::CantWrite)?;
+        write_file_name(out, &file)?;
         prefix += SPACER;
     }
 
@@ -100,7 +106,7 @@ fn print_file(
 }
 
 pub fn print_single_file(out: &mut io::StdoutLock, file: &File) -> Result<(), Errors> {
-    writeln!(out, "{}", file.name).map_err(|_| Errors::CantWrite)?;
+    write_file_name(out, &file)?;
 
     let len = file.lines.len();
     let mut i = 0;
@@ -112,5 +118,33 @@ pub fn print_single_file(out: &mut io::StdoutLock, file: &File) -> Result<(), Er
             writeln!(out, "{}{}", BRANCH_END, line).map_err(|_| Errors::CantWrite)?;
         }
     }
+    Ok(())
+}
+
+fn write_file_name(out: &mut io::StdoutLock, file: &File) -> Result<(), Errors> {
+    if let Some(linked) = &file.linked {
+        write!(out, "{}{}{}{}", FILE_COLOR, BOLD, file.name, STYLE_RESET)
+            .map_err(|_| Errors::CantWrite)?;
+        write!(out, " -> ").map_err(|_| Errors::CantWrite)?;
+        writeln!(
+            out,
+            "{}{}{}{}",
+            FILE_COLOR,
+            BOLD,
+            linked.to_string_lossy(),
+            STYLE_RESET
+        )
+        .map_err(|_| Errors::CantWrite)?;
+    } else {
+        writeln!(out, "{}{}{}{}", FILE_COLOR, BOLD, file.name, STYLE_RESET)
+            .map_err(|_| Errors::CantWrite)?;
+    }
+
+    Ok(())
+}
+
+fn write_dir_name(out: &mut io::StdoutLock, name: &String) -> Result<(), Errors> {
+    writeln!(out, "{}{}{}{}", DIR_COLOR, BOLD, name, STYLE_RESET).map_err(|_| Errors::CantWrite)?;
+
     Ok(())
 }
