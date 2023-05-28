e84ce2b75b739fd30d12c47b00bc35502c498cc2
Timothy Cronin <tcroniniv@gmail.com>
2023-05-28 13:07:03 -0400

    add more colors

--- src/formats.rs
+++ src/formats.rs
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 
+// Colors add 60 to the number for bright
 pub static RED_FG: &str = "\x1b[31m";
+pub static GREEN_FG: &str = "\x1b[32m";
+pub static YELLOW_FG: &str = "\x1b[33m";
 pub static BLUE_FG: &str = "\x1b[34m";
-pub static BOLD: &str = "\x1b[1m";
+pub static MAGENTA_FG: &str = "\x1b[35m";
+pub static CYAN_FG: &str = "\x1b[36m";
+
+// Other
 pub static RESET: &str = "\x1b[0m";
+pub static BOLD: &str = "\x1b[1m";
+// Printing
 pub static BRANCH_HAS_NEXT: &str = "├──";
 pub static BRANCH_END: &str = "└──";
 pub static VER_LINE_SPACER: &str = "│  ";
