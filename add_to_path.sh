aed88bba1409ea160c1e3278147ccfc8ee47d84d
Timothy Cronin <tcroniniv@gmail.com>
2023-05-28 20:47:54 -0400

    add source comp

--- add_to_path.sh
+++ add_to_path.sh
@@ -2,3 +2,28 @@
 cargo build --release
 # make this something in your path
 ln -sf $(pwd)/target/release/todo ~/bin/todo
+
+# Determine the type of shell
+SHELL_NAME=$(basename "$SHELL")
+
+# Source the corresponding completion file
+case "$SHELL_NAME" in
+  "bash")
+    source completions/todo.bash
+    ;;
+  "zsh")
+    source completions/_todo
+    ;;
+  "fish")
+    source completions/todo.fish
+    ;;
+  "elvish")
+    source completions/todo.elv
+    ;;
+  "powershell")
+    source _todo.ps1
+    ;;
+  *)
+    echo "Shell type not supported for sourcing completion file."
+    ;;
+esac
